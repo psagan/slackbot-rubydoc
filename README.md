bea7cda7b37df068623b8173c612250a0e0c377f
psagan <patryk.sagan@icloud.com>
2016-05-24 10:14:45 +0200

    Add descriptions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slackbot-rubydoc  [![Build Status](https://travis-ci.org/psagan/slackbot-rubydoc.svg?branch=master)](https://travis-ci.org/psagan/slackbot-rubydoc) [![Code Climate](https://codeclimate.com/github/psagan/slackbot-rubydoc/badges/gpa.svg)](https://codeclimate.com/github/psagan/slackbot-rubydoc)
-Slack bot for providing ruby documentation from ri CLI tool
+## Slack bot for providing ruby documentation from ri CLI tool
+
+
--- run.rb
+++ run.rb
@@ -1,11 +1,12 @@
 require_relative 'simple_autoloader'
 
-# config object
+# config object - environment variables loader
 config = Config::EnvLoader.new
 
+# instance of http communication class
 http_communication_class = HttpCommunication::NetHttp
 
-# slack client
+# slack client - I use Dependency Injection and aggregation (of external objects) approach here
 client = Client::Fye.new(
     config: config,
     rtm_start: Client::RtmStart.new(config: config, http_communication_class: http_communication_class),
@@ -13,6 +14,7 @@ client = Client::Fye.new(
     bot_params_class: Bot::Parameters
 )
 
+# start it!
 client.start
 
 
